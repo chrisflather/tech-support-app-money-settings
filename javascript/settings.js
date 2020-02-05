52073c0de78bd96ed9b0891d5cbb8f62c68cb612
github username <github email address>
2020-02-04 18:37:52 -0800

    Added some styling and javascript

--- javascript/settings.js
+++ javascript/settings.js
@@ -3782,3 +3782,16 @@ function displaySettings(cameraDisplaySpecialty) {
   secondDiv.appendChild(monitorSettings);
   monitorMainDiv.appendChild(secondDiv);
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  navToggle();
+});
+
+function navToggle() {
+  let mainNav = document.getElementById("js-menu");
+  let navBarToggle = document.getElementById("js-navbar-toggle");
+  navBarToggle.addEventListener("click", function() {
+    mainNav.classList.toggle("active");
+    this.classList.toggle("change");
+  });
+}
