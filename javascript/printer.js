57e6805384282d0ec97ddf8801e904e9def53ef8
github username <github email address>
2020-02-07 13:31:44 -0800

    Added event handler so I can grab the sdc, and assign its dataset to a variable

--- javascript/printer.js
+++ javascript/printer.js
@@ -6,8 +6,18 @@ const selectSDC = () => {
   let sdcButtons = document.getElementsByClassName("example_a");
   // loop over buttons and add event listener
   for (button of sdcButtons) {
-    button.addEventListener("click", () => {
-      console.log("click-click-boom");
+    button.addEventListener("click", function() {
+      // Grab the dataset using 'this' keyword
+      let sdc = this.dataset.sdc;
+      // I want to pass this camera to the 'showPrinters'
+      console.log(sdc);
+      showPrinters(sdc);
     });
   }
+};
+
+// Show printers
+const showPrinters = sdc => {
+  console.log(sdc);
+  alert(sdc);
 };
