cb9564419c494cf12ffac407aeda5ffd39ce5474
github username <github email address>
2020-02-07 14:11:01 -0800

    Added functionlity to the printer.js. Able to select sdc, and clear the dom.

--- javascript/printer.js
+++ javascript/printer.js
@@ -7,10 +7,14 @@ const selectSDC = () => {
   // loop over buttons and add event listener
   for (button of sdcButtons) {
     button.addEventListener("click", function() {
+      // Grab top div
+      sdcDiv = document.getElementById("sdc-div");
+      console.log(sdcDiv);
+      // Reassign the class name
+      sdcDiv.classList.add("printer-div");
       // Grab the dataset using 'this' keyword
       let sdc = this.dataset.sdc;
       // I want to pass this camera to the 'showPrinters'
-      console.log(sdc);
       showPrinters(sdc);
     });
   }
@@ -19,5 +23,4 @@ const selectSDC = () => {
 // Show printers
 const showPrinters = sdc => {
   console.log(sdc);
-  alert(sdc);
 };
