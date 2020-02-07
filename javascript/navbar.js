d703b26e433cfeb77b3eba796576dfe495143a89
github username <github email address>
2020-02-07 11:11:53 -0800

    Built out the printer page, with styling

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,9 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+  navToggle();
+});
+
 // Navbar functionality
-// function navToggle() {
-//   let mainNav = document.getElementById("js-menu");
-//   let navBarToggle = document.getElementById("js-navbar-toggle");
-//   navBarToggle.addEventListener("click", function() {
-//     mainNav.classList.toggle("active");
-//     this.classList.toggle("change");
-//   });
-// }
+function navToggle() {
+  let mainNav = document.getElementById("js-menu");
+  let navBarToggle = document.getElementById("js-navbar-toggle");
+  navBarToggle.addEventListener("click", function() {
+    mainNav.classList.toggle("active");
+    this.classList.toggle("change");
+  });
+}
--- javascript/settings.js
+++ javascript/settings.js
@@ -4270,17 +4270,3 @@ function displaySettings(cameraDisplaySpecialty) {
   secondDiv.appendChild(monitorSettings);
   monitorMainDiv.appendChild(secondDiv);
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-  navToggle();
-});
-
-// Navbar functionality
-// function navToggle() {
-//   let mainNav = document.getElementById("js-menu");
-//   let navBarToggle = document.getElementById("js-navbar-toggle");
-//   navBarToggle.addEventListener("click", function() {
-//     mainNav.classList.toggle("active");
-//     this.classList.toggle("change");
-//   });
-// }
