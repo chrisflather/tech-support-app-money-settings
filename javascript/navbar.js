015b6883e52573b9a060a1914d5223dbcff8e4a1
github username <github email address>
2020-04-08 11:51:56 -0700

    Updating master branch.

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,12 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   navToggle();
 });
 
 // Navbar functionality
 function navToggle() {
   let mainNav = document.getElementById("js-menu");
+  console.log(mainNav);
   let navBarToggle = document.getElementById("js-navbar-toggle");
-  navBarToggle.addEventListener("click", function() {
+  navBarToggle.addEventListener("click", function () {
     mainNav.classList.toggle("active");
     this.classList.toggle("change");
   });
